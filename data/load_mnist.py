3a2a98dc043a363f3267691e43b8d7afa6ddd3c9
probe05 <242503382@qq.com>
2025-12-25 20:10:24 +0800

    2025/12/25 data mnist and load_mnist

---
+++ data/load_mnist.py
@@ -0,0 +1,16 @@
+import gzip
+import numpy as np
+def load_mnist_images(filename):
+   with gzip.open(filename, 'rb') as f:
+       f.read(16)
+       data = np.frombuffer(f.read(), dtype=np.uint8).reshape(-1, 28, 28)
+   return data
+def load_mnist_labels(filename):
+   with gzip.open(filename, 'rb') as f:
+       f.read(8)
+       labels = np.frombuffer(f.read(), dtype=np.uint8)
+   return labels
+x_train = load_mnist_images('./MNIST/train-images-idx3-ubyte.gz')
+y_train = load_mnist_labels('./MNIST/train-labels-idx1-ubyte.gz')
+x_test = load_mnist_images('./MNIST/t10k-images-idx3-ubyte.gz')
+y_test = load_mnist_labels('./MNIST/t10k-labels-idx1-ubyte.gz')
